c1e86047bb7c2d5cfadf842fa76c7c9456bbb6d2
juan <juandans01@gmail.com>
2025-02-11 15:02:34 -0300

    update docker compose command

--- apps/api/deploy.sh
+++ apps/api/deploy.sh
@@ -2,9 +2,9 @@ echo "DEPLOY API"
 
 echo "build docker image"
 
-docker-compose -f docker-compose.api.yml build staging
+docker compose -f docker-compose.api.yml build staging
 
 
 echo "start docker image"
 
-docker-compose -f docker-compose.api.yml up -d staging
+docker compose -f docker-compose.api.yml up -d staging
