98b104af9bc9c8894920b93dcbb958882743277f
juan <juandans01@gmail.com>
2025-02-11 15:14:46 -0300

    simplify for the time being the deploy scripts

--- deploy-api.sh
+++ deploy-api.sh
@@ -2,15 +2,15 @@
 
 FOLDER="apps/api"
 
-echo "Checking for changes in $FOLDER..."
+# echo "Checking for changes in $FOLDER..."
 
-# check if there are changes in the last commit
-if git diff --quiet HEAD~1 -- "$FOLDER"; then
-  echo "No changes detected in $FOLDER"
-  exit 0
-else
-  echo "Changes detected in $FOLDER"
-fi
+# # check if there are changes in the last commit
+# if git diff --quiet HEAD~1 -- "$FOLDER"; then
+#   echo "No changes detected in $FOLDER"
+#   exit 0
+# else
+#   echo "Changes detected in $FOLDER"
+# fi
 
 
 echo "Starting deployment for $FOLDER..."
--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -2,13 +2,13 @@
 
 echo "Checking for changes in frontend folders..."
 
-# check if there are changes in the last commit
-if [git diff --quiet HEAD~1 -- "apps/public"] && [git diff --quiet HEAD~1 -- "apps/spaces"]; then
-  echo "No changes detected"
-  exit 0
-else
-  echo "Changes detected -> deploying..."
-fi
+# # check if there are changes in the last commit
+# if [git diff --quiet HEAD~1 -- "apps/public"] && [git diff --quiet HEAD~1 -- "apps/spaces"]; then
+#   echo "No changes detected"
+#   exit 0
+# else
+#   echo "Changes detected -> deploying..."
+# fi
 
 # build both apps 
-turbo build
+turbo run build --concurrency=1 
