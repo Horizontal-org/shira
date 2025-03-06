3d8382c9bc8f488e6960ab6ecc82cbf5bdd94431
juan <juandans01@gmail.com>
2025-03-06 17:29:59 -0300

    type harder questions table

---
+++ apps/api/src/migrations/1741292827994-alter_question_column_type_type.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AlterQuestionColumnTypeType1741292827994 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        // ALTER TABLE tablename MODIFY columnname INTEGER;
+        await queryRunner.query(`
+            ALTER TABLE questions
+            MODIFY type ENUM('demo', 'quiz')
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
