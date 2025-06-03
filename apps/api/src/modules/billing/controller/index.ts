28d769e7f7bc766bbdd2bcd5a8b1b51347e0882f
valbuenag <valbuena52@gmail.com>
2025-06-03 17:34:19 -0300

    add plans get endpoints

--- apps/api/src/modules/billing/controller/index.ts
+++ apps/api/src/modules/billing/controller/index.ts
@@ -1,5 +1,7 @@
 import { CreateSubscriptionController } from "./create-subscription.controller";
+import { ListPlanController } from "./list.plan.controller";
 
 export const billingControllers = [
-    CreateSubscriptionController
+    CreateSubscriptionController,
+    ListPlanController
 ]
