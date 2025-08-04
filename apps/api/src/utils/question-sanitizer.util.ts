16d390d1bc2cd2e8b7436a2b620993c56db6c80e
valbuenag <valbuena52@gmail.com>
2025-08-04 18:28:48 -0300

    add server-side-sanitization

---
+++ apps/api/src/utils/question-sanitizer.util.ts
@@ -0,0 +1,106 @@
+import * as sanitizeHtml from 'sanitize-html';
+
+export class QuestionSanitizer {
+  
+  static sanitizeQuestionContent(html: string): string {
+    if (!html) return '';
+    
+    return sanitizeHtml(html, {
+      // TipTap-compatible tags
+      allowedTags: [
+        // Basic formatting
+        'p', 'br', 'strong', 'em', 'u', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+        
+        // Lists
+        'ul', 'ol', 'li',
+        
+        // Tables
+        'table', 'thead', 'tbody', 'tfoot', 'tr', 'th', 'td', 'colgroup', 'col',
+        
+        // Other content
+        'blockquote', 'hr', 'a', 'span', 'div', 'mark',
+        
+        // Images
+        'img'
+      ],
+      
+      allowedAttributes: {
+        'a': ['href', 'title', 'target', 'rel'],
+        'img': ['src', 'alt', 'width', 'height', 'data-image-id', 'data-original-width', 'data-original-height', 'data-original-filename', 'data-explanation'],
+        'span': ['style', 'class', 'id', 'data-explanation'],
+        'div': ['style', 'class', 'id', 'data-position'],
+        'mark': ['data-explanation'],
+        
+        // Table attributes
+        'table': ['style', 'class', 'id'],
+        'thead': ['style', 'class'],
+        'tbody': ['style', 'class'],
+        'tfoot': ['style', 'class'],
+        'tr': ['style', 'class'],
+        'th': ['style', 'class', 'colspan', 'rowspan'],
+        'td': ['style', 'class', 'colspan', 'rowspan'],
+        'colgroup': ['style', 'class'],
+        'col': ['style', 'class'],
+        
+        // Allow class and id on all elements
+        '*': ['class', 'id']
+      },
+      
+      allowedStyles: {
+        '*': {
+          'color': [/^#[0-9a-f]{3,6}$/i],
+          'text-align': [/^(left|right|center|justify)$/],
+          'background-color': [/^#[0-9a-f]{3,6}$/i],
+          'width': [/^\d+px$/, /^\d+%$/, /^auto$/],
+          'height': [/^\d+px$/, /^\d+%$/, /^auto$/],
+          'min-width': [/^\d+px$/],
+          'min-height': [/^\d+px$/],
+          'border': [/^[\d\w\s#(),-]+$/],
+          'border-collapse': [/^(collapse|separate)$/],
+          'padding': [/^\d+px$/],
+          'margin': [/^\d+px$/],
+          'vertical-align': [/^(top|middle|bottom|baseline)$/]
+        }
+      },
+      
+      allowedSchemes: ['http', 'https', 'mailto'],
+      
+      disallowedTagsMode: 'discard',
+      
+      transformTags: {
+        'a': (tagName, attribs) => {
+          const href = attribs.href;
+          // Block javascript: and data: URLs
+          if (href && (href.startsWith('javascript:') || href.startsWith('data:'))) {
+            delete attribs.href;
+          }
+          return {
+            tagName: 'a',
+            attribs: {
+              ...attribs,
+              target: '_blank',
+              rel: 'noopener noreferrer'
+            }
+          };
+        }
+      }
+    });
+  }
+  
+  static hasXSSAttempt(content: string): boolean {
+    if (!content) return false;
+    
+    const dangerousPatterns = [
+      /<script[^>]*>/i,
+      /javascript:/i,
+      /on\w+\s*=/i,
+      /<iframe[^>]*>/i,
+      /<object[^>]*>/i,
+      /<embed[^>]*>/i,
+      /vbscript:/i,
+      /data:text\/html/i
+    ];
+    
+    return dangerousPatterns.some(pattern => pattern.test(content));
+  }
+}
