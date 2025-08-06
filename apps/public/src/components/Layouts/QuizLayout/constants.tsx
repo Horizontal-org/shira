cc9c8cba6d6687d266821db119a694dbf14f8e21
valbuenag <valbuena52@gmail.com>
2025-08-06 19:30:13 -0300

    add lang selection on custom quizz

---
+++ apps/public/src/components/Layouts/QuizLayout/constants.tsx
@@ -0,0 +1,22 @@
+export const LANG_OPTIONS = [
+  {
+    label: "English",
+    labelEnglish: "English",
+    value: "en",
+  },
+  {
+    label: "Español",
+    labelEnglish: "Spanish",
+    value: "es",
+  },
+  {
+    label: "Français",
+    labelEnglish: "French",
+    value: "fr",
+  },
+  {
+    label: "普通话",
+    labelEnglish: "Mandarin",
+    value: "cn",
+  },
+];
