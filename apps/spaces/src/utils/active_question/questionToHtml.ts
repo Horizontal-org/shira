981e3b90d3e66944b42f410f1387d6bbee92dde6
juan <juandans01@gmail.com>
2025-08-18 15:00:30 -0300

    remove comments

--- apps/spaces/src/utils/active_question/questionToHtml.ts
+++ apps/spaces/src/utils/active_question/questionToHtml.ts
@@ -1,56 +1,7 @@
-import { TextInput } from "@shira/ui";
-import { Explanation } from "../../store/slices/explanation";
 import { ActiveQuestion, QuestionDragEditor, QuestionDragImage, QuestionEditorInput, QuestionTextInput } from "../../store/types/active_question";
 
-
-// const getHtmlByType = (appType, questionContent) => {
-//     let keys = []
-//     if (appType === 'email') {
-//       keys.push(
-//         'component-required-sender-name',
-//         'component-required-sender-email',
-//         'component-optional-subject'
-//       )      
-//     } else if (appType === 'messaging') {
-//       keys.push(
-//         'component-required-fullname',
-//         'component-required-phone',
-//       )      
-//     }
-    
-//     return Object.keys(questionContent)
-//       .filter(c => keys.includes(c))
-//       .reduce((prev, current) => {
-//         return prev + questionContent[current]
-//       }, `<div id='type-content'>`) + '</div>'
-// }
-
-// const parseRequest = (question, explanations, quizId) => {
-//   let typeHtml = getHtmlByType(question.app.type, question.content)
-  
-//   // const requiredHTML = Object.keys(requiredContent).reduce((prev, current) => {
-//   //   return prev + requiredContent[current]
-//   // }, `<div id='required-content'>`) + '</div>'
-  
-//   // const optionalHTML = Object.keys(optionalContent).reduce((prev, current) => {
-//   //   return prev + optionalContent[current]
-//   // }, `<div id='optional-content'>`) + '</div>'
-
-//   const dynamicHTML = Object.keys(question.content)
-//     .filter(qk => qk.includes('component-text') || qk.includes('component-attachment') || qk.includes('component-image'))
-//     .reduce((prev, current) => {
-//       return prev + question.content[current]
-//     }, `<div id='dynamic-content'>`) + '</div>'
-
-
 export const activeQuestionToHtml = (activeQuestion: ActiveQuestion) => {
-  if (activeQuestion.app.type === 'email') {
-    // parse as email
-    return parseActiveQuestionToHtml(activeQuestion.content)  
-  } else {
-    return parseActiveQuestionToHtml(activeQuestion.content)
-    console.log('parse message')
-  }
+  return parseActiveQuestionToHtml(activeQuestion.content)  
 }
 
 const parseActiveQuestionToHtml = (content: Object) => {
@@ -74,14 +25,10 @@ const parseActiveQuestionToHtml = (content: Object) => {
     .reduce((prev, current) => {
       return prev + current
     }, `<div id='dynamic-content'>`) + '</div>'
-    
-  console.log("🚀 ~ parseEmailActiveQuestionToHtml ~ elementsArray:", elementsArray)
-  console.log("🚀 ~ parseEmailActiveQuestionToHtml ~ html:", html)
 
   return html
 }
 
-// `<span ${insertExplanation(expl)} id=component-optional-subject>${value}</span>` 
 const parseQuestionTextInput = (textInput: QuestionTextInput) => {
   const textElement = document.createElement('span')
   textElement.innerHTML = textInput.value
@@ -92,7 +39,6 @@ const parseQuestionTextInput = (textInput: QuestionTextInput) => {
   return textElement.outerHTML
 }
 
-// `<div data-position=1 id=component-text-1>${emailText}</div>`
 const parseQuestionEditorInput = (editorInput: QuestionEditorInput, returnType: 'string' | 'html' = 'string') => {
   const editorElement = document.createElement('div')
   editorElement.innerHTML = editorInput.value
@@ -115,15 +61,7 @@ const parseQuestionDragImage = (imageInput: QuestionDragImage) => {
   }
   return imageElement
 }
-// `<div data-position=${index} data-attachment-type=${ni.value.type} id=component-attachment-${index} ${insertExplanation(ni.explId || null)}>${ni.value.name || ''}</div>` 
-// const parseQuestionDragAttachmentInput = () => (dragAttachment: any) => {
-// }
 
-//       let objectAttributes = ''
-  //       if (imageObject) {
-  //         objectAttributes = `data-image-id=${imageObject.id} alt=${imageObject.originalFilename} src=${imageObject.url}`
-  //       }
-  //       newContent[`component-image-${index}`] = `<img data-position=${index} id=component-image-${index} ${insertExplanation(ni.explId || null)} ${objectAttributes} />` 
 export const parseDragItem = (item: QuestionDragEditor | QuestionDragImage) => {
   let element = null
   if (item.contentType === 'image') {
