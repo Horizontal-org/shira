ca2a681395b9d6684ea622670f2a632dedbaf120
fredzia <fredziaga@gmail.com>
2025-10-22 12:49:10 -0300

    fix: enhance accessibility by adding roles and aria-levels to headings in QuestionLibraryListLayout

--- apps/spaces/src/components/QuestionLibraryListLayout/index.tsx
+++ apps/spaces/src/components/QuestionLibraryListLayout/index.tsx
@@ -137,8 +137,8 @@ export const QuestionLibraryListLayout: FunctionComponent<Props> = ({ rows: rows
       <StyledBox>
         <HeaderRow>
           <div>
-            <H2>Question Library</H2>
-            <MiddleBody>
+            <H2 role="heading" aria-level={1}>Question Library</H2>
+            <MiddleBody role="heading" aria-level={2}>
               Select a question from list below to add it to your quiz. Once you've added it to your quiz, you can edit the question to fully customize it, including changing the text and explanations.
             </MiddleBody>
           </div>
@@ -219,7 +219,7 @@ const HeaderRow = styled("div")`
   color: ${defaultTheme.colors.dark.black};
 `;
 
-const Thead = styled.thead`
+const Thead = styled("thead")`
   & Th {
     background: ${defaultTheme.colors.light.paleGreen};
     &:first-child { border-top-left-radius: 20px; }
@@ -227,7 +227,7 @@ const Thead = styled.thead`
   }
 `;
 
-const Th = styled.th`
+const Th = styled("th")`
   text-align: left;
   padding: 14px 16px;
   font-weight: 600;
