9d4a43ed6e415629dc7a6fd970db573c43b29e89
fredzia <fredziaga@gmail.com>
2025-11-08 12:47:41 -0300

    feat: add LearnerQuiz entity to TypeOrmModule imports in LearnerModule

--- apps/api/src/modules/learner/learner.module.ts
+++ apps/api/src/modules/learner/learner.module.ts
@@ -4,13 +4,19 @@ import { Learner as LearnerEntity } from './domain/learner.entity';
 import { LearnerController } from './controllers/learner.controller';
 import { serviceLearnerProviders } from './learner.providers';
 import { SpaceEntity } from '../space/domain/space.entity';
+import { LearnerQuiz } from './domain/learners_quizzes.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([LearnerEntity, SpaceEntity]),
+    TypeOrmModule.forFeature([
+      LearnerEntity,
+      LearnerQuiz,
+      SpaceEntity
+    ]),
   ],
   controllers: [LearnerController],
   providers: [...serviceLearnerProviders],
   exports: [...serviceLearnerProviders],
 })
+
 export class LearnerModule { }
