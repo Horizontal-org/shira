88d9055a6d1c1cb34219cd39406271067c6e8624
fredzia <fredziaga@gmail.com>
2025-11-12 11:44:44 -0300

    feat: implement email invitation functionality and add template for learner invitation

--- apps/api/src/modules/learner/services/invite.learner.service.ts
+++ apps/api/src/modules/learner/services/invite.learner.service.ts
@@ -9,6 +9,8 @@ import { InviteEmailLearnerDto } from "../dto/invitation-email.learner.dto";
 import { EmailSendFailedException } from "../exceptions/email-send.learner.exception";
 import { SavingLearnerException as SaveLearnerException } from "../exceptions/save.learner.exception";
 import { ConflictLearnerException } from "../exceptions/conflict.learner.exception";
+import { Queue } from "bullmq";
+import { InjectQueue } from "@nestjs/bullmq";
 
 const UNIQUE_VIOLATION_CODE = '23505';
 
@@ -17,24 +19,10 @@ export class InviteLearnerService implements IInviteLearnerService {
   constructor(
     @InjectRepository(LearnerEntity)
     private readonly learnerRepo: Repository<LearnerEntity>,
-    private readonly sendInvitationAuthService: SendInvitationAuthService
+    private readonly sendInvitationAuthService: SendInvitationAuthService,
+    @InjectQueue('emails')
+    private emailsQueue: Queue
   ) { }
-
-  async sendInvitationEmail(inviteEmailLearnerDto: InviteEmailLearnerDto) {
-    const { email, spaceId } = inviteEmailLearnerDto;
-
-    console.debug("🚀 ~ InviteLearnerService ~ sendInvitationEmail ~ email:", email, "spaceId:", spaceId);
-
-    try {
-      await this.sendInvitationAuthService.execute({
-        email,
-        slug: 'learner-invitation', // TODO what slug to use?
-      });
-    } catch (err) {
-      console.error('Error sending learner invitation email', { email, spaceId, err });
-      throw new EmailSendFailedException();
-    }
-  }
 
   async invite(inviteLearnerDto: InviteLearnerDto) {
     const { email, spaceId, name, assignedByUser } = inviteLearnerDto;
@@ -56,6 +44,29 @@ export class InviteLearnerService implements IInviteLearnerService {
       if (err.code === UNIQUE_VIOLATION_CODE) throw new ConflictLearnerException();
 
       throw new SaveLearnerException();
+    }
+  }
+
+  async sendInvitationEmail(inviteEmailLearnerDto: InviteEmailLearnerDto) {
+    const { email, spaceId } = inviteEmailLearnerDto;
+
+    console.debug("InviteLearnerService ~ sendInvitationEmail ~ email:", email, "spaceId:", spaceId);
+
+    try {
+      await this.emailsQueue.add('send', {
+        to: email,
+        from: process.env.SMTP_GLOBAL_FROM,
+        subject: 'Invitation to create a Shira space',
+        template: 'learner-invitation',
+        data: {
+          email: email,
+          // magicLink: magicLink,
+          // passphrase: passphrase.code
+        }
+      })
+    } catch (err) {
+      console.error('InviteLearnerService ~ error sending learner invitation email', { email, spaceId, err });
+      throw new EmailSendFailedException();
     }
   }
 }
