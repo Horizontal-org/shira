a963b0dd3f0ed3b1a102dd2e519145baf848ca8f
fredzia <fredziaga@gmail.com>
2025-11-26 18:38:09 -0300

    refactor: improve error message clarity in AssignmentEmailSendFailedException

--- apps/api/src/modules/learner/exceptions/assignment-email-send.learner.exception.ts
+++ apps/api/src/modules/learner/exceptions/assignment-email-send.learner.exception.ts
@@ -3,7 +3,7 @@ import { LearnerErrorCodes } from "./errors/learner.error-codes";
 
 export class AssignmentEmailSendFailedException extends HttpException {
   constructor(email?: string) {
-    const cause = email ? `Failed to send quiz assignment email to learner ${email}`
+    const cause = email ? `Failed to send quiz assignment email to learner with email: ${email}`
       : "Failed to send quiz assignment email to learner";
     super(LearnerErrorCodes.EmailSendFailed, HttpStatus.INTERNAL_SERVER_ERROR, { cause });
   }
