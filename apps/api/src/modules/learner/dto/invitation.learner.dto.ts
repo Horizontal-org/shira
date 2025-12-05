218dfa5114ba59fc25a137a522d104d1b498c4c0
fredzia <fredziaga@gmail.com>
2025-12-05 12:14:07 -0300

    refactor: make name not required

--- apps/api/src/modules/learner/dto/invitation.learner.dto.ts
+++ apps/api/src/modules/learner/dto/invitation.learner.dto.ts
@@ -1,11 +1,11 @@
-import { IsEmail, IsNotEmpty, IsString, MaxLength } from "class-validator";
+import { IsEmail, IsString, MaxLength } from "class-validator";
 
 export class InviteLearnerDto {
   @IsEmail()
   email!: string;
 
-  @IsString() @IsNotEmpty() @MaxLength(255)
-  name!: string;
+  @IsString() @MaxLength(255)
+  name: string;
 
   assignedByUser?: number;
 }
