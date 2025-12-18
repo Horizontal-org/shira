1aa07a5162098dcffbb68049ea5d104aec52fc1c
fredzia <fredziaga@gmail.com>
2025-12-18 13:10:16 -0300

    refactor: add quizVisibility prop to TabContainer and conditionally render learners tab

--- apps/spaces/src/components/QuizViewLayout/index.tsx
+++ apps/spaces/src/components/QuizViewLayout/index.tsx
@@ -257,6 +257,7 @@ export const QuizViewLayout: FunctionComponent<Props> = () => {
                 quizId={quiz.id}
                 quizTitle={quiz.title}
                 quizQuestions={quiz.quizQuestions}
+                quizVisibility={quiz.visibility}
                 resultsData={resultsData}
                 resultsLoading={resultsLoading}
                 hasResults={hasResults}
