af2b9315a7196a523bf7f4c0839b0212d47422fa
fredzia <fredziaga@gmail.com>
2026-01-23 20:19:03 -0300

    feat: allow custom success message in handleCopyUrlAndNotify function

--- apps/spaces/src/utils/quiz.ts
+++ apps/spaces/src/utils/quiz.ts
@@ -1,9 +1,9 @@
 import toast from "react-hot-toast";
 
-export const handleCopyUrlAndNotify = async (hash: string) => {
+export const handleCopyUrlAndNotify = async (hash: string, successMessage?: string) => {
   try {
-    await handleCopyUrl(hash)
-    toast.success('The public quiz link has been copied to your clipboard.', { duration: 3000 })
+    await handleCopyUrl(hash);
+    toast.success(successMessage, { duration: 3000 });
   } catch (error) {
     console.error('Failed to copy URL:', error);
   }
