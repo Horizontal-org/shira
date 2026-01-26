50fa7b3648d76caad90c916bb849b786780f1a23
fredzia <fredziaga@gmail.com>
2026-01-26 15:37:14 -0300

    feat: add UnpublishQuizModal component for confirming quiz unpublishing

---
+++ apps/spaces/src/components/modals/UnpublishQuizModal/index.tsx
@@ -0,0 +1,47 @@
+import { FunctionComponent } from "react";
+import { Body1, Modal } from "@shira/ui";
+import styled from "styled-components";
+import { useTranslation } from "react-i18next";
+
+interface Props {
+  isModalOpen: boolean;
+  setIsModalOpen: (handle: boolean) => void;
+  onConfirm: () => void;
+  onCancel?: () => void;
+}
+
+export const UnpublishQuizModal: FunctionComponent<Props> = ({
+  isModalOpen,
+  setIsModalOpen,
+  onConfirm,
+  onCancel
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <Modal
+      id="unpublish-quiz-modal"
+      isOpen={isModalOpen}
+      title={t('modals.unpublish_quiz_with_questions.title')}
+      primaryButtonText={t('modals.unpublish_quiz_with_questions.confirm')}
+      secondaryButtonText={t('buttons.cancel')}
+      onPrimaryClick={() => {
+        setIsModalOpen(false);
+        onConfirm();
+      }}
+      onSecondaryClick={() => {
+        setIsModalOpen(false);
+        onCancel?.();
+      }}
+    >
+      <FormContent>
+        <Body1>{t('modals.unpublish_quiz_with_questions.message')}</Body1>
+      </FormContent>
+    </Modal>
+  )
+}
+
+const FormContent = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
