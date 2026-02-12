f15cd1d89e1059dbdb8a08960c938612ab4c9c28
fredzia <fredziaga@gmail.com>
2026-02-12 14:42:13 -0300

    chore(public): webpack-dev-server v5 config

--- apps/public/config/webpackDevServer.config.js
+++ apps/public/config/webpackDevServer.config.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const fs = require('fs');
-const evalSourceMapMiddleware = require('react-dev-utils/evalSourceMapMiddleware');
 const noopServiceWorkerMiddleware = require('react-dev-utils/noopServiceWorkerMiddleware');
 const ignoredFiles = require('react-dev-utils/ignoredFiles');
 const redirectServedPath = require('react-dev-utils/redirectServedPathMiddleware');
@@ -91,7 +89,15 @@ module.exports = function (proxy, allowedHost) {
       publicPath: paths.publicUrlOrPath.slice(0, -1),
     },
 
-    https: getHttpsConfig(),
+    server: (() => {
+      const httpsConfig = getHttpsConfig();
+
+      if (!httpsConfig) return 'http';
+      if (httpsConfig === true) return 'https';
+
+      // httpsConfig is { cert, key }
+      return { type: 'https', options: httpsConfig };
+    })(),
     host,
     historyApiFallback: {
       // Paths with dots should still use the history fallback.
@@ -101,27 +107,12 @@ module.exports = function (proxy, allowedHost) {
     },
     // `proxy` is run between `before` and `after` `webpack-dev-server` hooks
     proxy,
-    onBeforeSetupMiddleware(devServer) {
-      // Keep `evalSourceMapMiddleware`
-      // middlewares before `redirectServedPath` otherwise will not have any effect
-      // This lets us fetch source contents from webpack for the error overlay
-      devServer.app.use(evalSourceMapMiddleware(devServer));
-
-      if (fs.existsSync(paths.proxySetup)) {
-        // This registers user provided middleware for proxy reasons
-        require(paths.proxySetup)(devServer.app);
+    setupMiddlewares(middlewares, devServer) {
+      if (devServer?.app) {
+        devServer.app.use(redirectServedPath(paths.publicUrlOrPath));
+        devServer.app.use(noopServiceWorkerMiddleware(paths.publicUrlOrPath));
       }
-    },
-    onAfterSetupMiddleware(devServer) {
-      // Redirect to `PUBLIC_URL` or `homepage` from `package.json` if url not match
-      devServer.app.use(redirectServedPath(paths.publicUrlOrPath));
-
-      // This service worker file is effectively a 'no-op' that will reset any
-      // previous service worker registered for the same host:port combination.
-      // We do this in development to avoid hitting the production cache if
-      // it used the same host and port.
-      // https://github.com/facebook/create-react-app/issues/2272#issuecomment-302832432
-      devServer.app.use(noopServiceWorkerMiddleware(paths.publicUrlOrPath));
+      return middlewares;
     },
   };
 };
