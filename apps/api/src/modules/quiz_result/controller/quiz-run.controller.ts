15f5b14e2957080dc8e42a3c2e95ac7b44999721
fredzia <fredziaga@gmail.com>
2026-04-07 11:48:08 -0300

    refactor: replace AuthController decorator with Controller for QuizRunController

--- apps/api/src/modules/quiz_result/controller/quiz-run.controller.ts
+++ apps/api/src/modules/quiz_result/controller/quiz-run.controller.ts
@@ -1,13 +1,12 @@
-import { Body, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { TYPES } from '../interfaces';
 import { Inject } from '@nestjs/common';
 import { IStartQuizRunService } from '../interfaces/services/start-quiz-run.service.interface';
 import { IFinishQuizRunService } from '../interfaces/services/finish-quiz-run.service.interface';
 import { StartQuizRunDto } from '../dto/start-quiz-run.dto';
 import { FinishQuizRunDto } from '../dto/finish-quiz-run.dto';
-import { AuthController } from 'src/utils/decorators/auth-controller.decorator';
 
-@AuthController('quiz-run')
+@Controller('quiz-run')
 export class QuizRunController {
   constructor(
     @Inject(TYPES.services.IStartQuizRunService)
