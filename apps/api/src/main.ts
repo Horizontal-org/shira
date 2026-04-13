91f59174fef4c72e77da046b2083f04e47588700
= <=>
2026-04-13 16:41:41 -0300

    add cookie parser

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,14 +4,17 @@ import { ResponseNoCacheInterceptor } from './utils/interceptors/no-cache.interc
 import { ValidationPipe } from '@nestjs/common';
 import { ApiLogger } from './utils/logger/api-logger.service';
 import { LoggingInterceptor } from './utils/interceptors/logging.interceptor';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(IndexModule);
 
   const apiLogger = new ApiLogger();
 
+  app.use(cookieParser());
+
   app.enableCors({
-    origin: process.env.SPACE_URL,
+    origin: [process.env.SPACE_URL, process.env.PUBLIC_URL],
     credentials: true,
   });
 
