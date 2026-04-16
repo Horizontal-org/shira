d69ab356a17348de70376b102939aed9f7a02ebb
= <=>
2026-04-16 12:52:54 -0300

    remove legacy registration flow

--- apps/api/src/modules/auth/auth.provider.ts
+++ apps/api/src/modules/auth/auth.provider.ts
@@ -1,7 +1,5 @@
 import { TYPES } from './interfaces';
 import { ValidateAuthService, GenerateTokenAuthService } from './services';
-import { ConfirmRegistrationAuthService } from './services/confirm-registration.auth.service';
-import { SubmitRegistrationAuthService } from './services/submit-registration.auth.service';
 import { ValidateRegistrationAuthService } from './services/validate-registration.auth.service';
 import { SendInvitationAuthService } from './services/send-invitation.auth.service'
 import { SpaceRegistrationAuthService } from './services/space-registration.auth.service';
@@ -20,19 +18,9 @@ export const validateAuthServiceProvider = {
   useClass: ValidateAuthService,
 };
 
-export const submitRegistrationAuthServiceProvider = {
-  provide: TYPES.services.ISubmitRegistrationAuthService,
-  useClass: SubmitRegistrationAuthService,
-};
-
 export const validateRegistrationAuthServiceProvider = {
   provide: TYPES.services.IValidateRegistrationAuthService,
   useClass: ValidateRegistrationAuthService,
-};
-
-export const confirmRegistrationAuthServiceProvider = {
-  provide: TYPES.services.IConfirmRegistrationAuthService,
-  useClass: ConfirmRegistrationAuthService,
 };
 
 export const sendInvitationAuthServiceProvider = {
@@ -71,9 +59,7 @@ export const applicationsAuthProviders = [];
 export const servicesAuthProviders = [
   generateTokenAuthServiceProvider,
   validateAuthServiceProvider,
-  submitRegistrationAuthServiceProvider,
   validateRegistrationAuthServiceProvider,
-  confirmRegistrationAuthServiceProvider,
   sendInvitationAuthServiceProvider,
   spaceRegistrationAuthServiceProvider,
   requestPasswordResetAuthServiceProvider,
