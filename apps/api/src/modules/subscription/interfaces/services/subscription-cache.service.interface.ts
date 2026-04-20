e98b4cd886e9c50f43a873b9cfd02635a8d2d8a9
juandans01 <juandans01@gmail.com>
2026-04-20 11:14:07 -0300

    simplify query

--- apps/api/src/modules/subscription/interfaces/services/subscription-cache.service.interface.ts
+++ apps/api/src/modules/subscription/interfaces/services/subscription-cache.service.interface.ts
@@ -1,7 +1,7 @@
 import { CachedSubscription } from "../../dto/cached-response.dto"
 
 export interface ISubscriptionCacheService {
-    getCurrentSubscription(organizationId: string): Promise<CachedSubscription | null>
-    refresh(organizationId: string): Promise<CachedSubscription | null>
+    getCurrentSubscription(organizationId: string, spaceId: number): Promise<CachedSubscription | null>
+    refresh(organizationId: string, spaceId: number): Promise<CachedSubscription | null>
     invalidate(organizationId: string): Promise<void>
 }
