0b32fbd5ac0afb5a2faf606e726cdf097f20b45a
fredzia <fredziaga@gmail.com>
2026-06-04 22:45:35 -0300

    refactor

--- apps/api/src/modules/quiz/template/create-template.quiz.service.ts
+++ apps/api/src/modules/quiz/template/create-template.quiz.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, NotFoundException } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { EntityManager, Repository } from "typeorm";
 import * as crypto from "crypto";
