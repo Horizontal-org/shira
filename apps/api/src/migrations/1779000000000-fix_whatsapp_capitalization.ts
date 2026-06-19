645bf96793553ad97c131676340fa595d239469f
juandans01 <juandans01@gmail.com>
2026-06-19 15:47:08 -0300

    fix migration

--- apps/api/src/migrations/1779000000000-fix_whatsapp_capitalization.ts
+++ apps/api/src/migrations/1779000000000-fix_whatsapp_capitalization.ts
@@ -3,11 +3,11 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class FixWhatsappCapitalization1779000000000 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`UPDATE app SET name = 'WhatsApp' WHERE name = 'Whatsapp'`)
+    await queryRunner.query(`UPDATE apps SET name = 'WhatsApp' WHERE name = 'Whatsapp'`)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`UPDATE app SET name = 'Whatsapp' WHERE name = 'WhatsApp'`)
+    await queryRunner.query(`UPDATE apps SET name = 'Whatsapp' WHERE name = 'WhatsApp'`)
   }
 
 }
