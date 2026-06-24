d0ff8065af726ddff9b258cc93d6e492bd68a573
fredzia <fredziaga@gmail.com>
2026-06-24 12:23:04 -0300

    fix: update button text for quiz use template button

--- apps/spaces/src/components/modals/QuizLibraryPreviewModal/index.tsx
+++ apps/spaces/src/components/modals/QuizLibraryPreviewModal/index.tsx
@@ -151,7 +151,7 @@ export const QuizLibraryPreviewModal: FunctionComponent<Props> = ({
                 <ActionsDivider />
 
                 <Button
-                  text={t("dashboard.use_a_quiz_template_button")}
+                  text={t("quiz_library.preview.use_template")}
                   type="primary"
                   color={defaultTheme.colors.green7}
                   leftIcon={<FaCirclePlus size={16} />}
