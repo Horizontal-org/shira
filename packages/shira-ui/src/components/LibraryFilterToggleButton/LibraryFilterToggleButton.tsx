08bb96d32fa1f7469e7a9c93279d24ccb088bb92
fredzia <fredziaga@gmail.com>
2026-06-29 08:07:05 -0300

    refactor: remove FilterToggleButton component and update LibraryFilterToggleButton

--- packages/shira-ui/src/components/LibraryFilterToggleButton/LibraryFilterToggleButton.tsx
+++ packages/shira-ui/src/components/LibraryFilterToggleButton/LibraryFilterToggleButton.tsx
@@ -1,15 +1,50 @@
 import { FunctionComponent } from "react";
 import styled from "styled-components";
-import { FilterToggleButton, FilterToggleButtonProps } from "../FilterToggleButton";
+import { Button, defaultTheme } from "../..";
+import { MdFilterAlt } from "react-icons/md";
 
-export type LibraryFilterToggleButtonProps = FilterToggleButtonProps;
+export interface LibraryFilterToggleButtonProps {
+  text: string;
+  isOpen: boolean;
+  onClick: () => void;
+  className?: string;
+}
 
-export const LibraryFilterToggleButton: FunctionComponent<LibraryFilterToggleButtonProps> = (props) => {
-  return <StyledFilterToggleButton {...props} />;
+export const LibraryFilterToggleButton: FunctionComponent<LibraryFilterToggleButtonProps> = ({
+  text,
+  isOpen,
+  onClick,
+  className
+}) => {
+  return (
+    <FilterButton
+      className={className}
+      text={text}
+      type="outline"
+      leftIcon={<MdFilterAlt size={22} color={defaultTheme.colors.dark.darkGrey} />}
+      onClick={onClick}
+      $isOpen={isOpen}
+    />
+  );
 };
 
-const StyledFilterToggleButton = styled(FilterToggleButton)`
-  min-width: 144px;
+const FilterButton = styled(Button) <{ $isOpen: boolean }>`
+  border: ${props => (props.$isOpen ? '2px' : '1px')} solid ${props => (
+    props.$isOpen
+      ? props.theme.colors.dark.black
+      : props.theme.colors.dark.mediumGrey
+  )};
+  background: ${props => props.theme.colors.light.white};
+  color: ${props => props.theme.colors.dark.black};
+
+  &:focus {
+    margin: 0;
+    border: 2px solid ${props => (
+    props.$isOpen
+      ? props.theme.colors.dark.black
+      : props.theme.colors.dark.mediumGrey
+  )};
+  }
 
   @media (max-width: ${props => props.theme.breakpoints.md}) {
     flex: 1;
