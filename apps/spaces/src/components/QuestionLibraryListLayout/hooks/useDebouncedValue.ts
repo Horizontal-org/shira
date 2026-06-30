d98b0fee668700b79de25f37a2f8a51036cd5ac8
fredzia <fredziaga@gmail.com>
2026-06-30 17:59:04 -0300

    fix: loading skeleton debounce (#417)
    * fix: update default debounce delay to 500ms
    
    * fix: update loading skeleton logic to handle empty state
    
    * fix: remove unnecesary prop

--- apps/spaces/src/components/QuestionLibraryListLayout/hooks/useDebouncedValue.ts
+++ apps/spaces/src/components/QuestionLibraryListLayout/hooks/useDebouncedValue.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const DEFAULT_DEBOUNCE_DELAY_MS = 300;
+const DEFAULT_DEBOUNCE_DELAY_MS = 500;
 
 export const useDebouncedValue = (value: string, delay = DEFAULT_DEBOUNCE_DELAY_MS) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
