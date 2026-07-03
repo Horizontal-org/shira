0f256e6531553b94b6a679cf1862a55424b9eece
Agus <aguslasp@gmail.com>
2026-07-03 15:42:35 -0300

    feat(user): expose selfHosted flag in /user/me subscription response

--- apps/api/src/modules/user/controllers/me.user.controller.ts
+++ apps/api/src/modules/user/controllers/me.user.controller.ts
@@ -10,6 +10,7 @@ import { IMarkUserLoginService } from '../interfaces/services/mark.user.login.se
 import { SubscriptionGuard } from 'src/modules/subscription/guards/subscription.guard';
 import { SubscriptionDecorator } from 'src/modules/subscription/decorators/subscription.decorator';
 import { CachedSubscription } from 'src/modules/subscription/dto/cached-response.dto';
+import { SELF_HOSTED } from 'src/utils/environment/self-hosted.environment';
 
 @AuthController('user')
 export class MeUserController {
@@ -24,11 +25,11 @@ export class MeUserController {
   async me(
     @LoggedUser() user: LoggedUserDto,
     @SubscriptionDecorator() subscription: Partial<CachedSubscription>
-  ) {    
+  ) {
     await this.markUserLoginService.execute(user.id);
     return {
       user,
-      subscription
+      subscription: subscription ? { ...subscription, selfHosted: SELF_HOSTED } : subscription
     }
   }
 }
