4f8497c86bb1968f9a7f66dbcf337ded00c57e83
Agus <aguslasp@gmail.com>
2026-09-12 17:26:25 -0300

    fix: rtl icons sidebar

--- packages/shira-ui/src/components/Sidebar/useSidebar.tsx
+++ packages/shira-ui/src/components/Sidebar/useSidebar.tsx
@@ -25,7 +25,7 @@ const defaultAdminPaths = {
 
 const createDefaultMenuItems = (): MenuItem[] => [
   {
-    icon: <LuNotepadText id="dashboard-icon" size={24} color="currentColor" />,
+    icon: <LuNotepadText id="dashboard-icon" size={24} color="currentColor" data-mirror-rtl />,
     label: 'Quizzes',
     path: defaultAdminPaths.quizzes
   },
@@ -35,7 +35,7 @@ const createDefaultMenuItems = (): MenuItem[] => [
     path: defaultAdminPaths.learners
   },
   {
-    icon: <IoMdHelpCircle id="support-icon" size={24} color="currentColor" />,
+    icon: <IoMdHelpCircle id="support-icon" size={24} color="currentColor" data-mirror-rtl />,
     label: 'Support',
     path: defaultAdminPaths.support
   },
@@ -45,7 +45,7 @@ const createDefaultMenuItems = (): MenuItem[] => [
     path: defaultAdminPaths.settings
   },
   {
-    icon: <FiLogOut id="logout-icon" size={24} color="currentColor" />,
+    icon: <FiLogOut id="logout-icon" size={24} color="currentColor" data-mirror-rtl />,
     label: 'Log out',
     path: defaultAdminPaths.logout
   }
